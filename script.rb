ea8eb584a31b187cbff5234ea1a9dade7718d8e9
John Knapp <knappj2@gmail.com>
2023-06-12 20:58:32 -0400

    adding example script

---
+++ script.rb
@@ -0,0 +1,13 @@
+require 'discordrb'
+
+bot = Discordrb::Bot.new token: '<token>'
+
+# This method call adds an event handler that will be called on any message that exactly contains the string "Ping!".
+# The code inside it will be executed, and a "Pong!" response will be sent to the channel.
+bot.message(content: /pi.*/) do |event|
+  event.respond 'Pong!'
+end
+
+# This method call has to be put at the end of your script, it is what makes the bot actually connect to Discord. If you
+# leave it out (try it!) the script will simply stop and the bot will not appear online.
+bot.run
